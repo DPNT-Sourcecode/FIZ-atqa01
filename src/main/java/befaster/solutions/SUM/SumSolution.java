e59296be1757d96af2ace5d6eb1c3e837eeac272
sbx_user1060 <sbx_user1060@ip-10-128-89-64.eu-west-2.compute.internal>
2018-10-15 18:08:56 +0000

    Mon Oct 15 18:08:56 UTC 2018

--- src/main/java/befaster/solutions/SUM/SumSolution.java
+++ src/main/java/befaster/solutions/SUM/SumSolution.java
@@ -5,7 +5,7 @@ import befaster.runner.SolutionNotImplementedException;
 public class SumSolution {
 
     public int compute(int x, int y) {
-    	if( x < 0 || x > 100 || y > 0 || y > 100 ){
+    	if( x < 0 || x > 100 || y < 0 || y > 100 ){
     		throw new IllegalArgumentException("Input parameters out of bounds");
     	}
     	return x+y;
