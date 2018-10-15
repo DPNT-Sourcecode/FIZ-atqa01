fb58a8dccb3c0e706a93582f90280397b2b4752e
sbx_user1060 <sbx_user1060@ip-10-128-89-64.eu-west-2.compute.internal>
2018-10-15 17:58:25 +0000

    Mon Oct 15 17:58:25 UTC 2018

---
+++ src/main/java/befaster/solutions/SUM/SumSolutionTest.java
@@ -0,0 +1,14 @@
+package befaster.solutions.SUM;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SumSolutionTest {
+
+	@Test
+	public void testCompute() {
+		fail("Not yet implemented");
+	}
+
+}
