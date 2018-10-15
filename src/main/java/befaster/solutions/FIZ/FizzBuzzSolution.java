504252fc16ac6c8b91303a2ffd020b2197ba15b9
sbx_user1060 <sbx_user1060@ip-10-128-89-64.eu-west-2.compute.internal>
2018-10-15 18:27:00 +0000

    Mon Oct 15 18:27:00 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -5,22 +5,24 @@ import befaster.runner.SolutionNotImplementedException;
 public class FizzBuzzSolution {
 
     public String fizzBuzz(Integer number) {
-      
+       String output = "";
     	if(number > 9999 || number < 1){
     		throw new IllegalArgumentException("Input parameters out of bounds");
     	}
-    	if(number%3 == 0 && number%5 != 0) {
-    		return "fizz";
+    	if(number%3 == 0 || ("" + number).indexOf('3')!= -1) {
+    		
+    		output = output + "fizz";
     	}
-   
-    	if(number%3 != 0 && number%5 == 0) {
-    		return "buzz";
+    	if(number%5 == 0 || ("" + number).indexOf('5')!= -1) {
+    		if(output.equals("")) {
+    			output = "buzz";
+    		} else {
+    		output = output + " buzz";
+    		}
     	}
-    	if(number%3 == 0 && number%5 == 0) {
-    		return "fizz buzz";
-    	}
+
     	
-        return "" + number;
+        return output;
     	
    
    
