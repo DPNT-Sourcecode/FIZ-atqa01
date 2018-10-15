2821c31143c32a245b8e8238088602c1b45228d6
sbx_user1060 <sbx_user1060@ip-10-128-89-64.eu-west-2.compute.internal>
2018-10-15 19:02:37 +0000

    Mon Oct 15 19:02:37 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -10,16 +10,28 @@ public class FizzBuzzSolution {
     		throw new IllegalArgumentException("Input parameters out of bounds");
     	}
     	if(number%3 == 0 && ("" + number).indexOf('3')!= -1) {
-    		
-    		output = output + "fizz deluxe";
+    		if(number % 2 == 0) {
+    			output = "fizz deluxe";
+    		} else {
+    			output = "fizz fake deluxe";
+    		}
     	} else if(number%3 == 0 || ("" + number).indexOf('3')!= -1){
     		output = output + "fizz";
     	}
     	if(number%5 == 0 && ("" + number).indexOf('5')!= -1) {
+    		
     		if(output.equals("")) {
-    			output = "buzz deluxe";
+    			if(number % 2 == 0){
+    				output = "buzz deluxe";	
+    			} else {
+    				output = "buzz fake deluxe";
+    			}
     		} else {
-    		output = output + " buzz deluxe ";
+    			if(number % 2 == 0){ 
+    				output = output + " buzz deluxe";	
+    			} else {
+    				output = output + " buzz fake deluxe";
+    			}
     		}
     	} else if(number%5 == 0 || ("" + number).indexOf('5')!= -1) {
     		if(output.equals("")) {
@@ -28,37 +40,9 @@ public class FizzBuzzSolution {
     		output = output + " buzz";
     		}
     	}
-    	String num = "0123456789";
-    	int count = 0;
-    	for (int i = 0 ; i < num.length(); i++){
-    		if ( (""+number).indexOf(num.charAt(i)) !=-1){
-    			count++;
-    		}
-    	}
     	
-    	if (count == 1 && number > 10) {
-    		
-    		if(output.equals("")) {
-    			if(number % 2 == 0) {
-    				
-    			
-    			}else{
-    				output = "fake deluxe";
-    			}
-    		} else {
-    			if(number % 2 == 0) {
-    				
-    			
-    			} else {
-    				output = output + " fake deluxe";
-    			}
-    			
-    		}
-    		
-    		
-    	}
     	
-    	if(output.equals("") && (!(number%3 == 0 || ("" + number).indexOf('3')!= -1) && !(number%5 == 0 || ("" + number).indexOf('5')!= -1))) {
+    	if(output.equals("")) {
     		output = "" + number;
     	}
 
