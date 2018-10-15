ed0d56e940d53df168ee06f1521ff1e740d38212
sbx_user1060 <sbx_user1060@ip-10-128-89-64.eu-west-2.compute.internal>
2018-10-15 18:01:24 +0000

    Mon Oct 15 18:01:24 UTC 2018

--- src/main/java/befaster/solutions/SUM/SumSolutionTest.java
+++ src/main/java/befaster/solutions/SUM/SumSolutionTest.java
@@ -8,6 +8,15 @@ public class SumSolutionTest {
 
 	@Test
 	public void testCompute() {
+		SumSolution s = new SumSolution();
+		assertEquals(5,s.compute(3,2));
+		assertFalse(4 == s.compute(3, 2));
+		try {
+			assertEquals(101, s.compute(1,100));
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 		fail("Not yet implemented");
 	}
 
